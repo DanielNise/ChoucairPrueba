c6de8d2d16fe61694ea492c97a559eb472f810c7
Daniel Bolaños <felipesalazar651@gmail.com>
2022-04-04 14:59:04 -0500

    Update

---
+++ src/main/java/model/uTestData.java
@@ -0,0 +1,71 @@
+package model;
+
+public class uTestData {
+
+    private String strFirstName,strLastName,srtEmail,srtBirthMonth,strBirthDay,strBirthYear
+            ,strPassword,strConfirmPassword;
+
+    public String getStrFirstName() {
+        return strFirstName;
+    }
+
+    public void setStrFirstName(String strFirstName) {
+        this.strFirstName = strFirstName;
+    }
+
+    public String getStrLastName() {
+        return strLastName;
+    }
+
+    public void setStrLastName(String strLastName) {
+        this.strLastName = strLastName;
+    }
+
+    public String getSrtEmail() {
+        return srtEmail;
+    }
+
+    public void setSrtEmail(String srtEmail) {
+        this.srtEmail = srtEmail;
+    }
+
+    public String getSrtBirthMonth() {
+        return srtBirthMonth;
+    }
+
+    public void setSrtBirthMonth(String srtBirthMonth) {
+        this.srtBirthMonth = srtBirthMonth;
+    }
+
+    public String getStrBirthDay() {
+        return strBirthDay;
+    }
+
+    public void setStrBirthDay(String strBirthDay) {
+        this.strBirthDay = strBirthDay;
+    }
+
+    public String getStrBirthYear() {
+        return strBirthYear;
+    }
+
+    public void setStrBirthYear(String strBirthYear) {
+        this.strBirthYear = strBirthYear;
+    }
+
+    public String getStrPassword() {
+        return strPassword;
+    }
+
+    public void setStrPassword(String strPassword) {
+        this.strPassword = strPassword;
+    }
+
+    public String getStrConfirmPassword() {
+        return strConfirmPassword;
+    }
+
+    public void setStrConfirmPassword(String strConfirmPassword) {
+        this.strConfirmPassword = strConfirmPassword;
+    }
+}
